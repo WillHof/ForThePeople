c1fc0bc504321309af72d63c8be2544fe730ee1a
Will <whof@bu.edu>
2019-05-23 00:29:49 -0400

    senators display on the page

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database()
-
+var senators
 $(document).ready(function () {
     function compareSens(a, b) {
         // Use toUpperCase() to ignore character casing
@@ -41,11 +41,55 @@ $(document).ready(function () {
             dataType: "JSON",
             headers: { "X-API-Key": key },
         }).then(function (response) {
-            response.results[0].members.sort(compareSens)
-            console.log(response.results[0].members)
+            senators = response.results[0].members
+            senators.sort(compareSens)
+            console.log(senators)
+            createSenators(senators)
+
         });
     }
+    //     const senatorSkeleton = `<div class="card" style="width: 18rem;">
+    //     <div class="card-body">
+    //       <h5 class="card-title">${senators[i].first_name} ${senators[i].last_name}</h5>
+    //         <p class="card-text">test</p>
+    //     </div >
+    //         <ul class="list-group list-group-flush">
+    //             <li class="list-group-item">Cras justo odio</li>
+    //             <li class="list-group-item">Dapibus ac facilisis in</li>
+    //             <li class="list-group-item">Vestibulum at eros</li>
+    //         </ul>
+    //         <div class="card-body">
+    //             <a href="#" class="card-link">Card link</a>
+    //             <a href="#" class="card-link">Another link</a>
+    //         </div>
+    //   </div >`
+
+    function createSenators(obj) {
+        for (i = 0; i < obj.length; i++) {
+            $("#populateSenators").append(
+                `
+                <div class="card" style = "width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">${senators[i].first_name} ${senators[i].last_name} - ${senators[i].party}</h5>
+            <p class="card-text">State: ${senators[i].state}</p >
+        </div >
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Phone Number: ${senators[i].phone}</li>
+                    <li class="list-group-item">Date of Birth: ${senators[i].date_of_birth}</li>
+                    <li class="list-group-item">Next Election: ${senators[i].next_election}</li>
+                </ul>
+                <div class="card-body">
+                    <a href="${senators[i].url}" class="card - link">Website</a>
+                <a href ="${senators[i].contact_form}" class="card - link">Contact this Senator</a>
+                </div >
+          </div >
+                `);
+        }
+
+
+    }
     cKey()
+
 })
 
 
