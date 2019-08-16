d94636b56e9930fcb3fe2db858acff34b0644bf1
Will <whof@bu.edu>
2019-08-15 20:24:04 -0400

    more routes

--- routing/apiroutes.js
+++ routing/apiroutes.js
@@ -10,14 +10,32 @@ module.exports = function (app) {
         })
             .then(response => res.json(response.data.results[0].members)).catch(err => console.log(err))
     });
-    app.get("/api/getUpcoming", function (req, res) {
+    app.get("/api/getUpcomingH", function (req, res) {
         axios({
             method: 'get',
-            url: `https://api.propublica.org/congress/v1/bills/upcoming/${req}.json`,
+            url: `https://api.propublica.org/congress/v1/bills/upcoming/house.json`,
             headers: { 'X-API-Key': process.env.PROAPI },
-            dataType: "JSON",
+            dataType: "text/plain",
         })
-            .then(response => console.log(response)).catch(err => console.log(err))
+            .then(response => res.json(response.data.results[0].bills)).catch(err => console.log(err))
+    });
+    app.get("/api/getUpcomingS", function (req, res) {
+        axios({
+            method: 'get',
+            url: `https://api.propublica.org/congress/v1/bills/upcoming/senate.json`,
+            headers: { 'X-API-Key': process.env.PROAPI },
+            dataType: "text/plain",
+        })
+            .then(response => res.json(response.data.results[0].bills)).catch(err => console.log(err))
+    });
+    app.get("/api/getStatements", function (req, res) {
+        axios({
+            method: 'get',
+            url: `https://api.propublica.org/congress/v1/statements/latest.json`,
+            headers: { 'X-API-Key': process.env.PROAPI },
+            dataType: "text/plain",
+        })
+            .then(response => res.json(response.data.results)).catch(err => console.log(err))
     });
 }
 
