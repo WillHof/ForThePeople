6e52cdd9c182949d84cf99a42f174c1641637713
Will <whof@bu.edu>
2019-08-15 20:48:03 -0400

    added mongoose models for senators

---
+++ models/houseMembers.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+const HouseSchema = new Schema({
+    firstname: {
+        type: String,
+        trim: true,
+    },
+    lastname: {
+        type: String,
+        trim: true,
+    },
+    firstname: {
+        type: String,
+        trim: true,
+    },
+    birthdate: {
+        type: String,
+        trim: true,
+    },
+    gender: {
+        type: String,
+        trim: true,
+    },
+    party: {
+        type: String,
+        trim: true,
+    },
+    twitter: {
+        type: String,
+        trim: true,
+    },
+    facebook: {
+        type: String,
+        trim: true,
+    },
+    youtube: {
+        type: String,
+        trim: true,
+    },
+    website: {
+        type: String,
+        trim: true,
+    },
+    contact: {
+        type: String,
+        trim: true,
+    },
+    nextelection: {
+        type: String,
+        trim: true,
+    },
+    state:
+    {
+        type: String,
+        trim: true,
+    },
+    title: {
+        type: String,
+        trim: true,
+    },
+})
+const houseMember = mongoose.model("House", HouseSchema);
+module.exports = houseMember
