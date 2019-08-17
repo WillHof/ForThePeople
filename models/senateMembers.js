573fb7422503fb42498b8a3be9cb900971f4a12d
Will <whof@bu.edu>
2019-08-17 14:00:12 -0400

    scrapped mongoose, using SQL & sequelize

--- models/senateMembers.js
+++ models/senateMembers.js
@@ -1,63 +1,62 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema
-const SenateSchema = new Schema({
-    firstname: {
-        type: String,
-        trim: true,
-    },
-    lastname: {
-        type: String,
-        trim: true,
-    },
-    firstname: {
-        type: String,
-        trim: true,
-    },
-    birthdate: {
-        type: String,
-        trim: true,
-    },
-    gender: {
-        type: String,
-        trim: true,
-    },
-    party: {
-        type: String,
-        trim: true,
-    },
-    twitter: {
-        type: String,
-        trim: true,
-    },
-    facebook: {
-        type: String,
-        trim: true,
-    },
-    youtube: {
-        type: String,
-        trim: true,
-    },
-    website: {
-        type: String,
-        trim: true,
-    },
-    contact: {
-        type: String,
-        trim: true,
-    },
-    nextelection: {
-        type: String,
-        trim: true,
-    },
-    state:
-    {
-        type: String,
-        trim: true,
-    },
-    title: {
-        type: String,
-        trim: true,
-    },
-})
-const senateMember = mongoose.model("Senate", SenateSchema);
-module.exports = senateMember
+module.exports = function (sequelize, DataTypes) {
+    const SenateMembers = sequelize.define("Senate_Members", {
+        firstname: {
+            type: DataTypes.STRING,
+            trim: true,
+        },
+        lastname: {
+            type: DataTypes.STRING,
+            trim: true,
+        },
+        firstname: {
+            type: DataTypes.STRING,
+            trim: true,
+        },
+        birthdate: {
+            type: DataTypes.STRING,
+            trim: true,
+        },
+        gender: {
+            type: DataTypes.STRING,
+            trim: true,
+        },
+        party: {
+            type: DataTypes.STRING,
+            trim: true,
+        },
+        twitter: {
+            type: DataTypes.STRING,
+            trim: true,
+        },
+        facebook: {
+            type: DataTypes.STRING,
+            trim: true,
+        },
+        youtube: {
+            type: DataTypes.STRING,
+            trim: true,
+        },
+        website: {
+            type: DataTypes.STRING,
+            trim: true,
+        },
+        contact: {
+            type: DataTypes.STRING,
+            trim: true,
+        },
+        nextelection: {
+            type: DataTypes.STRING,
+            trim: true,
+        },
+        state:
+        {
+            type: DataTypes.STRING,
+            trim: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            trim: true,
+        },
+    })
+    return SenateMembers
+}
--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(express.json());
 app.use(express.static("public"));
 require("./routing/apiroutes")(app);
 require("./routing/htmlroutes")(app);
-app.listen(PORT, function () {
-    console.log("App listening at http://localhost:" + PORT)
+
+const db = require("./models")
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
+        console.log("App listening at http://localhost:" + PORT)
+    })
 })
