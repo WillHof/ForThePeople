38e5717049c25986e817b486a82f6d3e5480225d
Will <whof@bu.edu>
2019-08-29 18:20:08 -0400

    did the run build

---
+++ client/build/precache-manifest.fe2cd17a7db3d03ada758c49370e8aa6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da25463127a37cc59347f2e928181a0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9b335c968ae32d0ab5c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a5d195ac3dd112d8559d",
+    "url": "/static/js/2.7f294ba2.chunk.js"
+  },
+  {
+    "revision": "d9b335c968ae32d0ab5c",
+    "url": "/static/js/main.19196ea3.chunk.js"
+  },
+  {
+    "revision": "c451b177c877bbbf60cc",
+    "url": "/static/js/runtime~main.e043be84.js"
+  }
+]);
