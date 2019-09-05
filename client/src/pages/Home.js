4c174b001bb1c532224c9e325d2a4d322532edf1
Will <whof@bu.edu>
2019-09-05 13:39:17 -0400

    added splash page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
-import bgImage from "../assets/images/white_house.jpg"
 export class Home extends Component {
     render() {
         return (
             <div>
-                <img src={bgImage} className="background-image img-fluid" alt="Responsive"></img>
-            </div>
+                <div className="row" >
+                    <div className="jumbotron lucent" id="wrapper">
+                        <h1 className="font-script heading-style">For The People</h1>
+                        <hr className="my-4"></hr>
+                        <p className="lead text-center">Bringing transparency into what our government is actually doing</p>
+                    </div>
+                </div>
+
+
+            </div >
         )
     }
 }
